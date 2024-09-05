88f14e715899f82347cf093178d510cd3e6c3194
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-09-05 10:13:43 +0200

    first commit

---
+++ Compte.php
@@ -0,0 +1,10 @@
+<?php
+
+Class Compte{
+    private $_libell;
+    private $_solde;
+    private $_devise;
+    private $_titulaire;
+}
+
+public function __construct($libell, $solde, $devise, $titulaire)
