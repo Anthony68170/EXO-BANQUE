c793dd1ca4c7ac6d18d33e7bc5255abf8284ac03
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-09-05 11:23:33 +0200

    Suite Exo BANQUE POO_2 Comptes et Titulaire

--- Compte.php
+++ Compte.php
@@ -6,12 +6,50 @@ Class Compte{
     private $_solde;
     private $_devise;
     private $_titulaire;
-}
+    
+    public function __construct($libell, $solde, $devise, $titulaire){
+        $this->_libell = $libell;
+        $this -> _solde = $solde;
+        $this -> _devise = $devise;
+        $this -> _titulaire = $titulaire;
+        $titulaire -> ajouterCompte($this);
+    }
 
-public function __construct($libell, $solde, $devise, $titulaire){
-    $this->_libell = $libell;
-    $this -> _solde = $solde;
-    $this -> _devise = $devise;
-    $this -> _titulaire = $titulaire;
-    $titulaire -> ajouterCompte($this);
+    //GETTERS
+    public function getLibell(){
+        return $this -> _libell;
+    }
+
+    public function getSolde(){
+        return $this -> _solde;
+    }
+
+    public function getDevise(){
+        return $this -> _devise;
+    }
+
+    public function getTitulaire(){
+        return $this -> _titulaire;
+    }
+
+    // SETTERS
+    public function setLibell($libell){
+    return $this -> _libell;
+    }
+
+    public function setSolde(){
+    return $this -> _solde;
+    }
+
+    public function setDevise(){
+    return $this -> _devise;
+    }
+
+    public function setTitulaire(){
+    return $this -> _titulaire;
+    }
+
+    public function __toString(){
+    return $this->_libell . " : " . $this->_solde . "€. (" . $this->_devise . ") " . $this->_titulaire . ": ";
+    }
 }
--- Titulaire.php
+++ Titulaire.php
@@ -11,8 +11,32 @@ class Titulaire{
     public function __construct($prenom, $nom,){// Initialiser propriétés du TITULAIRE
         $this -> _prenom = $prenom;
         $this -> _nom = $nom;
+        $this -> _dateNaissance = $dateNaissance;
+        $this -> _ville = $ville;
         
-        //Stock les livres de l'auteur
-        $this->_livres = array();// le TABLEAU A INITILALISER VIDE de livre est à la base un tableai vide
+        //Stock les comptes du titulaire
+        $this->_comptes = array();// le TABLEAU A INITILALISER VIDE de livre est à la base un tableai vide
+    }
+
+    public function ajouterCompte($_comptes){
+$this -> _comptes[] = $compte;
+    }
+    public function afficherComptes(){ //PARCOURIR LES COMPTES DU TITULAIRE
+        // cete fonction fait ne boucle avec foreach sur les comptes du titulaire 
+        echo "Compte(s) de " .$this->$titulaire; // Afficher la phrase de présentation de l'afficahge des comptes
+        foreach ($this -> _comptes as $compte) { // Pour parcourir chaque compte du tableau
+            # code...
+            echo _comptes->__toString($compte); //Afficher le tableau COMPTES du titulaire
+        }
+
+        public function getPrenom(){
+            return $this->_prenom;
+          }
+          public function getNom(){
+            return $this->_nom;
+          }
+          public function __toString(){ //TOSTRING RETOURNE PRENOM ET NOM par la function public
+            return $this->_prenom . " " . $this->_nom;
+            }
     }
 }
